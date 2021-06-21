a534026ea6087a3dda58bd0f7ce4a1f8dd631c01
maj <446724946@qq.com>
2021-06-21 17:44:43 +0800

    Template

---
+++ src/template/FC.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import {} from 'antd';
+import {} from 'dva';
+import {} from 'umi';
+
+interface Props {}
+
+interface State {}
+
+type Student = {};
+
+export default (props: Props, state: State): JSX.Element => {
+    const [data, setData] = useState<Array<Student>>([]);
+
+    const load = (): void => {};
+
+    useEffect;
+
+    return <></>;
+};
---
+++ src/template/class.tsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import {} from 'antd';
+import {} from 'dva';
+import {} from 'umi';
+
+interface Props {}
+
+interface State {}
+
+class Temp extends Component<Props, State> {
+    public state: State = {};
+
+    public componentDidMount(): void {}
+
+    public render(): JSX.Element {
+        return <></>;
+    }
+}
+
+export default Temp;
